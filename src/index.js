ed3a55f93c86312fb821d97d55e34c61ace3c485
eduardofoliveira <eduardo_felipe_oliveira@yahoo.com.br>
2020-03-30 22:28:06 -0300

    remove .env

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 require('dotenv').config()
+const did = require('./model/didModel')
+
+const executar = async () => {
+  const result = await did.getDidsFromDomain({ domain: 'cloud.cloudcom.com.br' })
+}
+
+executar()
--- src/model/didModel.js
+++ src/model/didModel.js
@@ -5,11 +5,19 @@ class didModel {
     return new Promise((resolve, reject) => {
       try {
         const result = await connection.raw(`
-          SELECT
-            *
-          FROM
-            
+          select
+              *
+          from
+              tbl_pbx_address a,
+              tbl_sys_domain d
+          where
+              a.int_domain_key = d.int_domain_key and
+              a.int_type = 3 and
+              a.int_active != 0 and
+              d.vch_domain = '${domain}'
         `)
+
+        resolve(result)
       } catch (error) {
         reject(error)
       }
@@ -17,4 +25,4 @@ class didModel {
   }
 }
 
-module.exports = didModel()
+module.exports = new didModel()
